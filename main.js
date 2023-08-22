1b88787971d1aa9f9511243964b5c9c62fc8d317
Ana  Letícia <ti.leticia@ipti.org.br>
2023-08-22 16:44:56 -0300

    adicionar novo gato

--- main.js
+++ main.js
@@ -5,61 +5,38 @@ meuBody.style.display = "grid";
 meuBody.style.gridTemplateColumns = "1fr 1fr 1fr";
 meuBody.style.alignItems = "center";
 meuBody.style.backgroundColor = "#EFD3D7";
-  
-let meuTitulo = document.getElementById("totalcliques");
 
-
-//let meuh2 = document.getElementById("meuTitulo");
-
-
-// Função para gerar números inteiros aleatórios
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-const caract = ["lindo", "charmoso", "fofo", "brincalhão"];
-const gatos = [];
-for (let i=0; i<15; i++){
-  gatos[i] = caract[getRandomInt(0, 4)];
-}
-
-let total = 0;
-for (let i = 0; i < 5; i++) {
+const addCat = () => {
   let count = 0;
   let meuContainer = document.createElement("div");
   meuContainer.classList.add("container");
-  meuContainer.style.background ="rgba( 255, 255, 255, 0.30 )";
-  meuContainer.style.backdropFilter ="blur(16px)";
+  meuContainer.style.background = "rgba( 255, 255, 255, 0.30 )";
+  meuContainer.style.backdropFilter = "blur(16px)";
   meuContainer.style.width = "20vw";
   meuContainer.style.display = "flex";
-  meuContainer.style.flexDirection="column";
-  meuContainer.style.justifyContent="center";
-  meuContainer.style.alignItems="center";
+  meuContainer.style.flexDirection = "column";
+  meuContainer.style.justifyContent = "center";
+  meuContainer.style.alignItems = "center";
   meuContainer.style.gap = "2px";
   meuContainer.style.padding = "15px";
   meuContainer.style.margin = "10px";
- 
- 
- 
+
   let minhaImg = document.createElement("img");
-  minhaImg.src = `https://loremflickr.com/320/240/cat/?random=${i}`;
+  minhaImg.src = `https://loremflickr.com/320/240/cat/?random=${gatos.length}`;
   minhaImg.style.width = "150px";
   minhaImg.style.height = "150px";
-  minhaImg.style.borderRadius="50%";
+  minhaImg.style.borderRadius = "50%";
   minhaImg.style.gap = "10px";
   meuContainer.appendChild(minhaImg);
 
-
   let quantcliques = document.createElement("h4");
-  //quantcliques.className ="quantcliquestotais";
   quantcliques.textContent = "0";
   meuContainer.appendChild(quantcliques);
 
   let gatoescolhido = document.createElement("p");
-  gatoescolhido.textContent = gatos[i];
+  gatoescolhido.textContent = gatos[gatos.length - 1];
   meuContainer.appendChild(gatoescolhido);
-  console.log(gatoescolhido.textContent);
- 
+
   let meuBotao = document.createElement("button");
   meuBotao.className = "meuBotao";
   meuBotao.textContent = "Doe aqui!";
@@ -68,12 +45,11 @@ for (let i = 0; i < 5; i++) {
   meuBotao.style.alignContent = "center";
   meuBotao.style.alignItems = "center";
   meuBotao.style.borderRadius = "5px";
-  meuBotao.style.border = "none";  
+  meuBotao.style.border = "none";
   meuBotao.style.width = "100px";
   meuBotao.style.height = "40px";
 
   meuContainer.appendChild(meuBotao);
-
 
   let meuBotao2 = document.createElement("button");
   meuBotao2.className = "meuBotao2";
@@ -83,34 +59,61 @@ for (let i = 0; i < 5; i++) {
   meuBotao2.style.alignContent = "center";
   meuBotao2.style.alignItems = "center";
   meuBotao2.style.borderRadius = "5px";
-  meuBotao2.style.border = "none";  
+  meuBotao2.style.border = "none";
   meuBotao2.style.width = "100px";
   meuBotao2.style.height = "40px";
   meuContainer.appendChild(meuBotao2);
-
 
   meuBotao.addEventListener("click", function () {
     count++;
     quantcliques.textContent = count;
   });
-  
 
-  let botoes = document.querySelectorAll('.meuBotao');    
   meuBotao.addEventListener("click", () => {
-      total++;    
-      meuTitulo.textContent = total;
+    total++;
+    meuTitulo.textContent = total;
   });
 
   meuBotao2.addEventListener("click", function () {
-    //let countcat = meuContainer.querySelector(".quantcliquestotais").innerHTML;
     let countcat = quantcliques.textContent;
-    meuTitulo.textContent = parseInt(meuTitulo.textContent) - parseInt(countcat);
+    meuTitulo.textContent =
+      parseInt(meuTitulo.textContent) - parseInt(countcat);
     total = meuTitulo.textContent;
-    meuContainer.remove(); 
-  
+    meuContainer.remove();
   });
 
   meuBody.appendChild(meuContainer);
-  
+  gatos.push(caract[getRandomInt(0, 10)]);
+};
+
+let meuTitulo = document.getElementById("totalcliques");
+
+let adicionarGato = document.getElementById("adicionarGato");
+adicionarGato.addEventListener("click", addCat);
+
+// Função para gerar números inteiros aleatórios
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
+const caract = [
+  "lindo",
+  "charmoso",
+  "fofo",
+  "brincalhão",
+  "carinhoso",
+  "tímido",
+  "Mega fofo",
+  "Querendo um humano",
+  "muito fofinho também",
+  "Mais fofo ainda",
+];
+const gatos = [];
+for (let i = 0; i < 10; i++) {
+  gatos[i] = caract[getRandomInt(0, 10)];
+}
+
+let total = 0;
+for (let i = 0; i < 10; i++) {
+  addCat();
+}
